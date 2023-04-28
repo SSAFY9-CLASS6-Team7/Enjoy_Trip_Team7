2303c4f4ba1ab2a448b76b1efdfed405654015e5
Junyoung <wnsdud12365@naver.com>
2023-04-28 23:06:59 +0900

    Feat : 게시글 수정, 삭제 기능 구현

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/board/model/service/BoardService.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/board/model/service/BoardService.java
@@ -4,9 +4,14 @@ import com.ssafy.enjoytrip.board.model.Board;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 public interface BoardService {
-    List<Board> getBoardList() throws SQLException;
+    List<Board> getBoardList(Map<String, String> paramMap) throws SQLException;
 
     void createBoard(Board board) throws SQLException;
+
+    void updateBoard(Board board) throws SQLException;
+
+    void deleteBoard(int boardId) throws SQLException;
 }
