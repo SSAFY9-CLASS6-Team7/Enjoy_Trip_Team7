e94eebadfc09d7dc908e80b6a95d914dd2127fe0
Junyoung <wnsdud12365@naver.com>
2023-05-01 16:10:51 +0900

    Feat : ImageMapper 선언 추가

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/image/model/mapper/ImageMapper.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/image/model/mapper/ImageMapper.java
@@ -4,9 +4,17 @@ import com.ssafy.enjoytrip.image.model.Image;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface ImageMapper {
+	List<Image> selectImage(Map<String, Integer> paramMap) throws SQLException;
+
 	void insertImage(Image image) throws SQLException;
+
+	void deleteImage(int imageId) throws SQLException;
+
+	void cascadeDeleteImage(Map<String, Integer> paramMap) throws SQLException;
 
 }
