234b0b968af5268a21b43cea996267c15f44357b
dayounglee <dayoung015@gmail.com>
2023-05-15 04:08:58 +0900

    Feat : 기록 추가용 모달창
    라우터와의 연결방안을 마련해야

--- frontend/src/components/history/HistoryCreate.vue
+++ frontend/src/components/history/HistoryCreate.vue
@@ -1,9 +1,48 @@
 <template>
-  <div>
-    <h2>기록 추가 페이지</h2>
+  <div class="modal">
+    <div class="overlay" @click="modalOpen = false"></div>
+    <div class="modal-card">
+      <h2>기록 추가 페이지</h2>
+    </div>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "HistoryCreate",
+  data() {
+    return {
+      modalOpen: false,
+    };
+  },
+};
+</script>
 
-<style></style>
+<style scoped>
+.modal,
+.overlay {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 10;
+}
+
+.overlay {
+  opacity: 0.5;
+  background-color: black;
+}
+
+.modal-card {
+  position: relative;
+  min-width: 70%;
+  min-height: 70%;
+  border-radius: 30px;
+  background-color: white;
+  z-index: 11;
+}
+</style>
