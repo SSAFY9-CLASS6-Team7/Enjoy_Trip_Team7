a640edc08504d86533b6309280268c3191746b10
dayounglee <dayoung015@gmail.com>
2023-05-24 07:09:46 +0900

    Feat : 카카오맵 마커 띄우기 수정

--- frontend/src/components/TheKakaoMap.vue
+++ frontend/src/components/TheKakaoMap.vue
@@ -14,34 +14,30 @@ export default {
       map: null,
       positions: [],
       markers: [],
+      travelSido: 0,
     };
   },
   props: {
     groupedAttractionsArray: [], //날짜별 관광지들
     focused: Number, //몇번째 day를 보고 있는지
+    plan: Object, //여행에 대한 정보 -> 대표여행지get
+  },
+  computed: {
+    sidoCode() {
+      return this.$store.getters.getSidoCode;
+    },
+    SidoLatLng() {
+      const sido = this.sidoCode.find((item) => item.code === String(this.travelSido));
+      const lat = sido ? sido.lat : 33.450701;
+      const lng = sido ? sido.lng : 126.570667;
+      return [lat, lng];
+    },
   },
   watch: {
-    async focused() {
-      //   console.log('attractions', this.groupedAttractionsArray);
-      //   console.log('focused Index : ', this.focused);
-      //   console.dir(this.groupedAttractionsArray[this.focused - 1]);
-
-      this.positions = [];
-      this.groupedAttractionsArray[this.focused - 1].forEach((attraction) => {
-        var targetAttraction = {};
-        axios.get('http://localhost/attraction/' + attraction.attractionId).then((res) => {
-          console.dir(res.data);
-          targetAttraction = res.data;
-        });
-        // console.dir(targetAttraction);
-        let obj = {};
-        obj.title = targetAttraction.title;
-        obj.latlng = new kakao.maps.LatLng(targetAttraction.lat, targetAttraction.lng);
-
-        this.positions.push(obj);
+    focused() {
+      this.$nextTick(() => {
+        this.loadData();
       });
-    //   console.dir(this.positions);
-      this.loadMaker();
     },
   },
   created() {},
@@ -52,13 +48,26 @@ export default {
     } else {
       this.loadScript();
     }
-    // console.log('attractions', this.groupedAttractionsArray);
-    // console.log('focused Index : ', this.focused);
-    // console.dir(this.groupedAttractionsArray[this.focused - 1]);
   },
   methods: {
-    loadData(){
+    async loadData() {
+      this.travelSido = this.plan.travelArea;
+      this.positions = [];
+      await Promise.all(
+        this.groupedAttractionsArray[this.focused - 1].map(async (attraction) => {
+          var targetAttraction = {};
+          const res = await axios.get('http://localhost/attraction/' + attraction.attractionId);
+          // console.dir(res.data);
+          targetAttraction = res.data;
 
+          let obj = {};
+          obj.title = targetAttraction.title;
+          obj.latlng = new kakao.maps.LatLng(targetAttraction.lat, targetAttraction.lng);
+
+          this.positions.push(obj);
+        })
+      );
+      this.loadMaker();
     },
 
     // api 불러오기
@@ -77,25 +86,24 @@ export default {
     loadMap() {
       const container = document.getElementById('map');
       const options = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        center: new window.kakao.maps.LatLng(this.SidoLatLng[0], this.SidoLatLng[1]),
         level: 3,
       };
 
       this.map = new window.kakao.maps.Map(container, options);
       //   this.loadMaker();
+      this.loadData();
     },
     // 지정한 위치에 마커 불러오기
     loadMaker() {
       // 현재 표시되어있는 marker들이 있다면 marker에 등록된 map을 없애준다.
-      console.log('1111');
       this.deleteMarker();
-      console.log('2222');
       // 마커 이미지를 생성합니다
       //TODO: 임시로 이미지 넣어둠
-      const imgSrc = require('@/assets/plan_icon/pin.svg');
+      // const imgSrc = require('@/assets/plan_icon/pin.svg');
       // 마커 이미지의 이미지 크기 입니다
-      const imgSize = new kakao.maps.Size(24, 35);
-      const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize);
+      // const imgSize = new kakao.maps.Size(24, 35);
+      // const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize);
 
       // 마커를 생성합니다
       this.markers = [];
@@ -104,7 +112,7 @@ export default {
           map: this.map, // 마커를 표시할 지도
           position: position.latlng, // 마커를 표시할 위치
           title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-          image: markerImage, // 마커의 이미지
+          // image: markerImage, // 마커의 이미지
         });
         this.markers.push(marker);
       });
@@ -112,18 +120,20 @@ export default {
 
       // 4. 지도를 이동시켜주기
       // 배열.reduce( (누적값, 현재값, 인덱스, 요소)=>{ return 결과값}, 초기값);
-      const bounds = this.positions.reduce(
-        (bounds, position) => bounds.extend(position.latlng),
-        new kakao.maps.LatLngBounds()
-      );
+      if (this.markers.length > 0) {
+        const bounds = this.positions.reduce(
+          (bounds, position) => bounds.extend(position.latlng),
+          new kakao.maps.LatLngBounds()
+        );
 
-      this.map.setBounds(bounds);
+        this.map.setBounds(bounds);
+      }
     },
     deleteMarker() {
       console.log('마커 싹 지우자!!!', this.markers.length);
       if (this.markers.length > 0) {
         this.markers.forEach((item) => {
-          console.log(item);
+          // console.log(item);
           item.setMap(null);
         });
       }
